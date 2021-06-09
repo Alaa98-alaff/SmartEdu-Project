546b4f086b75b96f831cfd4ea2ebd87ddd028081
DESKTOP-7KRFCKA\trave <travel98_alaff@hotmail.com>
2021-06-10 02:14:55 +0300

    routing update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const pageRouter = require('./routes/pageRoutes');
 //templat engine
 app.set('view engine', 'ejs');
 
@@ -8,17 +8,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 //Routes
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-    page_name: 'index',
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.status(200).render('about', {
-    page_name: 'about',
-  });
-});
+app.use('/', pageRouter);
 
 const port = 3000;
 app.listen(port, () => {
