da5c99cada31a12a5396121a93ecb3f2d35a851f
DESKTOP-7KRFCKA\trave <travel98_alaff@hotmail.com>
2021-06-09 21:08:21 +0300

    setting up the project

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 
+//templat engine
+app.set('view engine', 'ejs');
+
+//MiddleWares
+app.use(express.static('public'));
+
+//Routes
 app.get('/', (req, res) => {
-  res.status(200).send('index.html');
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
 });
 
 const port = 3000;
